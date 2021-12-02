0d1fee00234789b90a887e47ac518e31f0263195
KKS <kirill.kuliuckin2016@yandex.ru>
2021-12-02 19:48:28 +0300

    Добавлен веб-сервер

---
+++ TestServer/src/main/java/Ontology.java
@@ -0,0 +1,144 @@
+import org.apache.jena.ontology.Individual;
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.query.*;
+import org.apache.jena.rdf.model.InfModel;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.reasoner.Reasoner;
+import org.apache.jena.reasoner.rulesys.GenericRuleReasoner;
+import org.apache.jena.reasoner.rulesys.Rule;
+import org.apache.jena.riot.RDFDataMgr;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import static org.apache.jena.ontology.OntModelSpec.OWL_MEM_MICRO_RULE_INF;
+
+public class Ontology {
+    protected static String FLY_ONTOLOGY_FILE = "fly.owl";
+
+    protected Model model = null;
+    protected OntModel inf = null;
+    Reasoner reasoner = null;
+    InfModel infModel = null;
+
+    protected Model readModel(String modelFile)
+    {
+        // create an empty model
+        Model model = ModelFactory.createDefaultModel();
+        String inputFileName=modelFile;
+        // use the RDFDataMgr to find the input file
+        InputStream in = RDFDataMgr.open( inputFileName );
+        if (in == null) {
+            throw new IllegalArgumentException(
+                    "File: " + inputFileName + " not found");
+        }
+
+        // read the RDF/XML file
+        model.read(in, null);
+        return model;
+    }
+
+    protected static String readStream(InputStream is) {
+        StringBuilder sb = new StringBuilder(512);
+        try {
+            Reader r = new InputStreamReader(is, "UTF-8");
+            int c = 0;
+            while ((c = r.read()) != -1) {
+                sb.append((char) c);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return sb.toString();
+    }
+
+    public Ontology()
+    {
+       model = readModel(FLY_ONTOLOGY_FILE);
+
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("fly.rules");
+        String rules = readStream( stream);
+        //System.out.print(rules);
+        reasoner = new GenericRuleReasoner(Rule.parseRules(rules));
+        reasoner.setDerivationLogging(true);
+        reasoner.setDerivationLogging(true);
+        reasoner.bindSchema(model);
+
+       inf = ModelFactory.createOntologyModel( OWL_MEM_MICRO_RULE_INF, model);
+    }
+
+    public Resource addObject(String id)
+    {
+        String queryString = "PREFIX fo: <http://www.semanticweb.org/dns/ontologies/2021/10/fly#> " +
+                "SELECT ?o " +
+                " WHERE { "+
+                "?o fo:hasID  \"" + id +"\"" +
+                " }";
+
+        //String queryString = "SELECT ?problem ?parameter WHERE {?problem <http://www.semanticweb.org/problem-ontology#hasParameter> ?parameter }";
+        Query query = QueryFactory.create(queryString);
+        //InfModel infModel = ModelFactory.createInfModel(reasoner, inf);
+        QueryExecution qExec = QueryExecutionFactory.create(query, inf);
+        ResultSet rs = qExec.execSelect();
+        if (! (rs.hasNext()) ) {
+
+            Individual fly = inf.createIndividual(inf.createResource());
+            fly.addOntClass(inf.getOntClass("http://www.semanticweb.org/dns/ontologies/2021/10/fly#PhysicalObject"));
+            fly.addProperty(inf.createDatatypeProperty("http://www.semanticweb.org/dns/ontologies/2021/10/fly#hasID"), id);
+            fly.addProperty(inf.createDatatypeProperty("http://www.semanticweb.org/dns/ontologies/2021/10/fly#hasSpeed"), inf.createTypedLiteral(0.001) );
+            fly.addProperty(inf.createDatatypeProperty("http://www.semanticweb.org/dns/ontologies/2021/10/fly#hasAceleration"), inf.createTypedLiteral(0.005) );
+            fly.addProperty(inf.createDatatypeProperty("http://www.semanticweb.org/dns/ontologies/2021/10/fly#isCalculated"), inf.createTypedLiteral(false) );
+            fly.addProperty(inf.createDatatypeProperty("http://www.semanticweb.org/dns/ontologies/2021/10/fly#needAcelerate"), inf.createTypedLiteral(false));
+        }
+
+        while (rs.hasNext())
+        {
+            Resource fly = rs.next().get("?o").asResource();
+            return fly;
+        }
+        return null;
+    }
+
+    public float getSpeed(String id, boolean accelerate)
+    {
+        Resource fly = addObject(id);
+        //fly.getProperty(inf.getDatatypeProperty("http://www.semanticweb.org/dns/ontologies/2021/10/fly#hasNewSpeed")).remove();//.changeLiteralObject((double) 0);
+        infModel = ModelFactory.createInfModel(reasoner, inf);
+
+        //float v1 = fly.getProperty(inf.getDatatypeProperty("http://www.semanticweb.org/dns/ontologies/2021/10/fly#hasSpeed")).getFloat();
+        //return v1;
+
+        String queryString = "PREFIX fo: <http://www.semanticweb.org/dns/ontologies/2021/10/fly#> " +
+                "SELECT ?v ?a" +
+                " WHERE { " +
+                "?o a fo:PhysicalObject ."+
+                "?o fo:hasID  \"" + id +"\" ." +
+                "?o fo:hasSpeed ?v ." +
+                "?o fo:hasAceleration ?a ." +
+                " }";
+
+
+        Query query = QueryFactory.create(queryString);
+        QueryExecution qExec = QueryExecutionFactory.create(query, infModel);
+        ResultSet rs = qExec.execSelect();
+        if (rs.hasNext())
+        {
+            QuerySolution qs = rs.next();
+            float v = qs.get("?v").asLiteral().getFloat();
+            float a = qs.get("?a").asLiteral().getFloat();
+            fly.getProperty(inf.getDatatypeProperty("http://www.semanticweb.org/dns/ontologies/2021/10/fly#hasSpeed")).changeLiteralObject(v);
+            fly.getProperty(inf.getDatatypeProperty("http://www.semanticweb.org/dns/ontologies/2021/10/fly#hasAceleration")).changeLiteralObject(a);
+            fly.getProperty(inf.getDatatypeProperty("http://www.semanticweb.org/dns/ontologies/2021/10/fly#needAcelerate")).changeLiteralObject((accelerate));
+
+            System.out.println(accelerate ? "acelerate" : "not acelerate");
+            return v;
+        }
+        return 0;
+
+    }
+
+}
---
+++ TestServer/src/main/java/Test.java
@@ -0,0 +1,97 @@
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import org.apache.commons.lang3.StringEscapeUtils;
+import sun.misc.IOUtils;
+
+import org.json.JSONObject;
+
+public class Test {
+
+    private static Ontology o = new Ontology();
+
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/test", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    private static String createResponse(String requestMethod) {
+        String resp = "<svg viewbox='-10 -10 20 20' height='100vh'><text text-" +
+                "anchor='middle' stroke='red' stroke-width='0.05' font-size='4'>%s</text></svg>";
+        if ("GET".equals(requestMethod))
+            return String.format(resp, "GET");
+        if ("POST".equals(requestMethod))
+            return String.format(resp, "POST");
+        return "unknown method";
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            float speed = 0;
+            o.addObject("1");
+
+            //String text = problem.getFullText();
+            //System.out.println(text);
+
+            String method = t.getRequestMethod();
+
+            System.out.println(t.getRequestBody().toString());
+            byte [] b = new byte[1000];
+            InputStream is = t.getRequestBody();
+
+            int ch;
+            StringBuilder sb = new StringBuilder();
+            while((ch = is.read()) != -1)
+                sb.append((char)ch);
+            String inputString = sb.toString();
+            System.out.println("String: "+inputString);
+            if (sb.length()>0) {
+
+                HashMap<String, String> param_value = new HashMap<String, String>();
+                String[] params = inputString.split("&");
+                for (String param : params) {
+                    String[] key_value = param.split("=");
+                    param_value.put(key_value[0], key_value[1]);
+                }
+
+                String pressed = param_value.get("pressed");
+                System.out.println("Pressed: "+pressed);
+                speed = o.getSpeed("1", (pressed=="true"));
+            }
+
+
+            String response = "This is the response";
+
+            OutputStream os = t.getResponseBody();
+            OutputStreamWriter ow = new OutputStreamWriter(os, "Cp1251");
+            String respStr = new String(response.getBytes("ISO-8859-1"), "Cp1251");
+            JSONObject jo = new JSONObject();
+            jo.put("text", StringEscapeUtils.escapeJava(response));
+            jo.put("speed", speed);
+            String jsonStr = jo.toString();
+            t.sendResponseHeaders(200, jsonStr.length());
+            ow.write(jsonStr);
+            ow.flush();
+            ow.close();
+            //os.write(response.getBytes());
+            //os.close();
+        }
+
+        private String requestToJena(HashMap<String, String> param_value)
+        {
+            return "Correct";
+        }
+
+    }
+}
